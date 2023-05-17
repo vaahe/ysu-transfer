7f291773ea78fb33f39921bcc8fd6c13967e7209
vaahe <vahebarseghyan9999@gmail.com>
2023-05-17 17:48:37 +0400

    fix guardians

--- frontend/src/styles/ConnectWallet.js
+++ frontend/src/styles/ConnectWallet.js
@@ -1,4 +1,4 @@
 export const styles = {
-    background: "bg-[url('https://e-learning.ysu.am/pluginfile.php/1/theme_moove/sliderimage1/1684004547/moodle%201.png')]"
+    background: "bg-[url('https://e-learning.ysu.am/pluginfile.php/1/theme_moove/sliderimage1/1684004547/moodle%201.png')] bg-no-repeat bg-center bg-fixed bg-clip-border bg-cover"
 
 }
