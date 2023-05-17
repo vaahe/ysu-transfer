0213440f19c229c7b874aa75d98e42212f1aabb8
vaahe <vahebarseghyan9999@gmail.com>
2023-05-17 17:31:16 +0400

    hide sensitive data

--- frontend/src/components/ConnectWallet.js
+++ frontend/src/components/ConnectWallet.js
@@ -1,30 +1,27 @@
 import React from "react";
 
+import { styles } from "../styles/ConnectWallet";
+
 import { NetworkErrorMessage } from "./messages/NetworkErrorMessage";
 
 export function ConnectWallet({ connectWallet, networkError, dismiss }) {
   return (
-    <div className="container">
-      <div className="row justify-content-md-center">
-        <div className="col-12 text-center">
-          {/* Wallet network should be set to Localhost:8545. */}
-          {networkError && (
-            <NetworkErrorMessage
-              message={networkError}
-              dismiss={dismiss}
-            />
-          )}
-        </div>
-        <div className="col-6 p-4 text-center">
-          <p>Please connect to your wallet.</p>
-          <button
-            className="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-2xl px-6 py-3 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55"
-            type="button"
-            onClick={connectWallet}
-          >
-            Connect Wallet
-          </button>
-        </div>
+    <div className={`${styles.background} h-screen
+    `}>
+      <div className="text-center">
+        {networkError && (
+          <NetworkErrorMessage
+            message={networkError}
+            dismiss={dismiss}
+          />
+        )}
+        <p>Please connect to your wallet.</p>
+        <button
+          className="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-2xl px-6 py-3 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55"
+          onClick={connectWallet}
+        >
+          Connect Wallet
+        </button>
       </div>
     </div>
   );
---
+++ frontend/src/styles/ConnectWallet.js
@@ -0,0 +1,4 @@
+export const styles = {
+    background: "bg-[url('https://e-learning.ysu.am/pluginfile.php/1/theme_moove/sliderimage1/1684004547/moodle%201.png')]"
+
+}
